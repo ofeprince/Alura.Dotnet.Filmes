4a82de505bd6bd7f3c934e689371a7a41bd8de6a
Fellipe Prince <lfellipe.prince@gmail.com>
2024-03-25 21:46:01 -0300

    Adicao de modelo e validacoes com data annotation

---
+++ Alura.Dotnet.Filmes.Api/Controllers/FilmeController.cs
@@ -0,0 +1,28 @@
+﻿using Alura.Dotnet.Filmes.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alura.Dotnet.Filmes.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> _filmes = new List<Filme>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return  Ok(_filmes);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Filme filme)
+        {
+            _filmes.Add(filme);
+
+            Console.WriteLine(filme);
+
+            return Created();
+        }
+    }
+}
---
+++ Alura.Dotnet.Filmes.Api/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alura.Dotnet.Filmes.Api.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "Titulo do filme obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Genero { get; set; }
+
+        [Required]
+        [Range(40, 600)]
+        public int Duracao { get; set; }
+
+        public override string ToString()
+        {
+            return $"Titulo: {Titulo}, Genero: {Genero}, duracao: {Duracao}";
+        }
+    }
+}
